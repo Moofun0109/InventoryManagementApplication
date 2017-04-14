4f61224808fd1fc20c90504e3620e4c44d142ba6
Moofun0109 <Jerry.kelsay@astontech.com>
2017-04-14 09:42:49 -0500

    First commit

---
+++ FakeProject/src/main/java/FakeProject/FakeProjectApplication.java
@@ -0,0 +1,12 @@
+package FakeProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeProjectApplication.class, args);
+	}
+}
